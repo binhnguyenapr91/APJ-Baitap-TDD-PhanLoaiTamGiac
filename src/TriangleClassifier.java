97063644f622bd0c4f1639f92229dabcabb4b62c
binhnguyenapr91 <binhnguyen.apr91@gmail.com>
2020-03-31 22:04:12 +0700

    finish

--- src/TriangleClassifier.java
+++ src/TriangleClassifier.java
@@ -1,18 +1,31 @@
-import static java.lang.Math.*;
-import static java.lang.Math.pow;
-
 public class TriangleClassifier {
     public String classifier(Triangle triangle) {
-        if (triangle.getSide1() + triangle.getSide2() > triangle.getSide3() && triangle.getSide1() + triangle.getSide3() > triangle.getSide2() && triangle.getSide2() + triangle.getSide3() > triangle.getSide1()) {
-            if (triangle.getSide1() == triangle.getSide2() && triangle.getSide2() == triangle.getSide3()) {
+        if (isTriangle(triangle)) {
+            if (isEquilateralTriangle(triangle)) {
                 return "This is Equilateral Triangle";
-            } else if (triangle.getSide1() == triangle.getSide2() || triangle.getSide1() == triangle.getSide3() || triangle.getSide2() == triangle.getSide3()) {
+            } else if (isIsoscelesTriangle(triangle)) {
                 return "This is Isosceles Triangle";
-            } else if (Math.pow(triangle.getSide1(), 2) + Math.pow(triangle.getSide2(), 2) == Math.pow(triangle.getSide3(), 2) || Math.pow(triangle.getSide1(), 2) + Math.pow(triangle.getSide3(), 2) == Math.pow(triangle.getSide2(), 2) || Math.pow(triangle.getSide2(), 2) + Math.pow(triangle.getSide3(), 2) == Math.pow(triangle.getSide1(), 2)) {
+            } else if (isRightTriangle(triangle)) {
                 return "This is Right Triangle";
             } else {
                 return "This is Triangle";
             }
         } else return "This is not Triangle";
+    }
+
+    private boolean isRightTriangle(Triangle triangle) {
+        return Math.pow(triangle.getSide1(), 2) + Math.pow(triangle.getSide2(), 2) == Math.pow(triangle.getSide3(), 2) || Math.pow(triangle.getSide1(), 2) + Math.pow(triangle.getSide3(), 2) == Math.pow(triangle.getSide2(), 2) || Math.pow(triangle.getSide2(), 2) + Math.pow(triangle.getSide3(), 2) == Math.pow(triangle.getSide1(), 2);
+    }
+
+    private boolean isTriangle(Triangle triangle) {
+        return triangle.getSide1() + triangle.getSide2() > triangle.getSide3() && triangle.getSide1() + triangle.getSide3() > triangle.getSide2() && triangle.getSide2() + triangle.getSide3() > triangle.getSide1();
+    }
+
+    private boolean isIsoscelesTriangle(Triangle triangle) {
+        return triangle.getSide1() == triangle.getSide2() || triangle.getSide1() == triangle.getSide3() || triangle.getSide2() == triangle.getSide3();
+    }
+
+    private boolean isEquilateralTriangle(Triangle triangle) {
+        return triangle.getSide1() == triangle.getSide2() && triangle.getSide2() == triangle.getSide3();
     }
 }
